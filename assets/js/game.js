7dd9124888a07b25d9663cbac11f8130256074ac
Robby <n.robinson@Robinsons-MBP.cable.rcn.com>
2021-12-03 10:56:30 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,9 +69,12 @@ var fight = function(enemyName) {
   }
 };
 
+var startGame = function() {
 // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
+  //play again
+  startGame();
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
@@ -87,10 +90,13 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
-  }
+    }
   // if player isn't alive, stop the game
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
+    }
   }
 }
+
+startGame()
